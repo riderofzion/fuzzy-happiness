3a2b9fd546eb7eca51b0dd7ad1f90188d0660ade
stropee <stropee@eni-ecole.fr>
2024-01-12 16:45:42 +0100

    TP6

--- app/src/main/java/com/example/eni_shop/ui/HomeActivity.kt
+++ app/src/main/java/com/example/eni_shop/ui/HomeActivity.kt
@@ -1,12 +1,35 @@
 package com.example.eni_shop.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.eni_shop.R
+import com.example.eni_shop.ui.addarticle.AddArticleActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val title = intent.getStringExtra("title")
+        val price = intent.getStringExtra("price")
+
+        if (title != null && price != null) {
+            Snackbar.make(
+                findViewById<FloatingActionButton>(R.id.floatingActionButton),
+                "Vous venez de créer ${title} pour un montant de ${price}",
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
+
+
+
+        findViewById<FloatingActionButton>(R.id.floatingActionButton).setOnClickListener {
+            Intent(this, AddArticleActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/eni_shop/ui/addarticle/AddArticleActivity.kt
+++ app/src/main/java/com/example/eni_shop/ui/addarticle/AddArticleActivity.kt
@@ -1,11 +1,14 @@
 package com.example.eni_shop.ui.addarticle
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.eni_shop.R
+import com.example.eni_shop.bo.Article
 import com.example.eni_shop.databinding.ActivityAddArticleBinding
 import com.example.eni_shop.repository.ArticleRepository
+import com.example.eni_shop.ui.HomeActivity
 import com.google.android.material.snackbar.Snackbar
 
 class AddArticleActivity : AppCompatActivity() {
@@ -15,16 +18,21 @@ class AddArticleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_add_article)
 
-        //val article = Article()
-        val article = ArticleRepository.getArticle(1)
+        val article = Article()
+        //val article = ArticleRepository.getArticle(1)
         binding.article = article
 
         binding.button.setOnClickListener {
-            Snackbar.make(
-                it,
-                "Vous venez de créer ${article?.titre} pour un montant de ${article?.prix}",
-                Snackbar.LENGTH_LONG
-            ).show()
+
+            val id = ArticleRepository.addArticle(article)
+
+            if(id != null){
+                Intent(this, HomeActivity::class.java).also {
+                    it.putExtra("title", article.titre)
+                    it.putExtra("price", article.prix.toString())
+                    startActivity(it)
+                }
+            }
 
 //            Toast.makeText(this,
 //                "Vous venez de créer ${article.titre} pour un montant de ${article.prix}",
